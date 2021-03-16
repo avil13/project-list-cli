90b17b11beae953c4e148fb7b3750c8dac8cd0fa
avil13 <13.avil@gmail.com>
2021-03-17 01:53:07 +0300

    wip: change base script

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import { logMessages } from './lib/log/log-messages';
 import { addCommand } from './commands/add';
 import { lsCommand } from './commands/ls';
 import { rmCommand } from './commands/rm';
 import { checkAndGetConfig } from './lib/config-action/check-and-get-config';
 import { writeConfig } from './lib/config-action/write-config';
+import { log } from './lib/log/log';
 import { TMainArgs } from './types';
 
 // Если нет глобального конфига, предлагаем создать
@@ -15,12 +17,15 @@ const main = async (arg: TMainArgs = 'ls') => {
   const conf = await checkAndGetConfig();
   const pwd = process.cwd();
 
-  if (arg === 'ls') {
+  if (arg === 'ls' || arg === undefined) {
     await lsCommand(conf);
   } else if (arg === 'rm') {
     await rmCommand(conf, pwd);
   } else if (arg === 'add') {
     await addCommand(conf, pwd);
+  } else {
+    log.help(logMessages.help);
+    return;
   }
 
   await writeConfig(conf);
@@ -31,7 +36,9 @@ export { main };
 const isMainProcess = require.main === module;
 
 if (isMainProcess) {
-  main()
+  const [arg] = process.argv.slice(2);
+
+  main(arg as TMainArgs)
     .catch((err) => {
       console.error(err);
       process.exit(1);
